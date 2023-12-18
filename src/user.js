7bdb5ffd44bc90025ad88e56de1f3d59a1716fc4
Gurpreet Singh <52376160+GurpreetxSingh90@users.noreply.github.com>
2023-12-18 15:26:01 +0000

    user , image and preddiction classes are added

---
+++ src/user.js
@@ -0,0 +1,17 @@
+class User {
+  constructor(name, email, age) {
+    this.name = name
+    this.email = email
+    this.age = age
+  }
+}
+
+class Image {
+  constructor(path, format, height, width) {
+    this.path = path
+  }
+}
+
+class Prediction {
+  constructor(image, text) {}
+}
