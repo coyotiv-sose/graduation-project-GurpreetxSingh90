4c5beb360ba293e49fbd896164a77a03bef31dd8
Gurpreet Singh <52376160+GurpreetxSingh90@users.noreply.github.com>
2024-01-04 12:09:25 +0000

    Add dotenv configuration and establish database connection in app.js

--- src/app.js
+++ src/app.js
@@ -8,6 +8,8 @@ var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
 
 var app = express()
+require('dotenv').config()
+require('./database-connection')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
